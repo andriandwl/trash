28ec668982dc24a41b6caad0d8b770528da48992
aguspriyo <agusppriiyono@gmail.com>
2022-11-20 02:12:10 +0700

    new

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -31,20 +31,21 @@ const About = () => {
               sampah, demi kenyamanan dan kebersihan di lingkungan kehidupan Masyarakat.
             </p>
           </div>
-          <div className="col col-xl-3 col-12justify-content-right space-right2 ">
+          <div className="col col-xl-3 col-12 justify-content-right ">
             <img src={pohon} width="" className="float-end img-fluid " alt="gambar" style={{ opacity: "60%" }} />
           </div>
         </div>
       </div>
 
-      <section className=" " style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", padding: "7rem", marginTop: "-2em" }}>
+      <section className=" " style={{ backgroundImage: `url(${bg})`, backgroundSize: "cover", backgroundRepeat: "no-repeat", padding: "10rem"}}>
         <div className="container ">
           <div className="row ">
-            <div className=" mt-5 col-md-12 col-sm-12 col-lg-6 text-start text-black p-5">
-              <h4 className="" style={{ fontFamily: "museo" }}>
-                Mari Bersama Mewujudkan Jawa barat Nyaman dan Asri dengan..
+            <div className="col  col-12 col-xl-6  text-black mt-5 p-5">
+              <h4 className="text-center " style={{ fontFamily: "museo" }}>
+                Mari Bersama Mewujudkan Linkungan Nyaman dan Asri dengan..
               </h4>
-              <div className="col-12 gap-2 d-flex mt-2 justify-content-start" style={{ fontFamily: "Manrope" }}>
+              <div className="row">
+              <div className="col gap-2 d-flex mt-2 " style={{ fontFamily: "Manrope" }}>
                 <img className="" src={mark2} alt="mark" style={{ width: "40px", objectFit: "contain" }} />
                 <p className="mt-4  fw-medium">
                   Melestarikan Lingkungan.
@@ -52,8 +53,9 @@ const About = () => {
                   <small>Salah satu contoh sikap bertanggung jawab menjaga lingkungan adalah menjaga kelestarian lingkungan. Meningkatkan pemanfaatan sumber-sumber energi yang enggak akan habis.</small>
                 </p>
               </div>
-
-              <div className="col-12 gap-2 d-flex mt-2 justify-content-start">
+              </div>
+              <div className="row">
+              <div className="col gap-2 d-flex mt-2 ">
                 <img className="" src={mark1} alt="mark" style={{ width: "40px", objectFit: "contain" }} />
 
                 <p className="mt-4  fw-medium">
@@ -61,8 +63,9 @@ const About = () => {
                   <small>Menjaga lingkungan dapat dilakukan dengan cara mengurangi sampah, menghemat penggunaan listrik, menghemat penggunaan air, melestarikan hutan, dan melestarikan hewan.</small>
                 </p>
               </div>
-
-              <div className="col gap-2 d-flex mt-2 justify-content-start">
+              </div>
+              <div className="row">
+              <div className="col gap-2 d-flex mt-2 ">
                 <img className="" src={mark3} alt="mark" style={{ width: "40px", objectFit: "contain" }} />
 
                 <p className="mt-4  fw-medium">
@@ -71,9 +74,10 @@ const About = () => {
                   <small>Sebagai upaya menjaga kelestarian lingkungan, Anda dapat memisahkan sampah menjadi sampah anorganik, sampah organik, dan sampah kaca atau logam.</small>
                 </p>
               </div>
+              </div>
             </div>
-            <div className="col-md-12 col-sm-12 col-lg-6">
-              <div className="col-auto  d-lg-flex justify-content-end">
+            <div className="col  col-12 col-xl-6">
+              <div className="col-auto  d-flex justify-content-end">
                 <img className="img-fluid" src={trash} alt="" width="" />
               </div>
             </div>
@@ -81,17 +85,7 @@ const About = () => {
         </div>
       </section>
 
-      {/* <section className="position-relative">
-        <div className="container container-fluid text-center mt-4 p-5">
-          <img src={bg} alt="" style={{ marginTop: "100", position: "absolute", top: "0", left: "0", width: "100%", height: "100%", objectFit: "cover", margin: "0", padding: "0px" }} />
-          <div className="row py-lg-5 position-sticky">
-            <div className="col-lg-6 col-md-8 mx-auto p-2">
 
-              <p className="lead text-light">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
-            </div>
-          </div>
-        </div>
-      </section> */}
 
       <div className="container ">
         <div className="row text-center text-black mb-10 ">
