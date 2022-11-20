662fe5106f689dcf7c557f1f4ba4ded35fa03382
aguspriyo <agusppriiyono@gmail.com>
2022-11-20 22:32:11 +0700

    set profile page

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,7 +5,7 @@ const Profile = () => {
   return (
     <>
     <Navigation/>
-    <section className="">
+    <section className="profile">
       <div className="container bg-light">
         <div className="row">
           <div className="col-lg-12 mb-4 mb-sm-5">
