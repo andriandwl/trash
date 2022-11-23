d86e3efb307250ec775987e532d076c57208c6a9
Andrian Dwi Haryanto <76521228+andriandwl@users.noreply.github.com>
2022-11-24 00:33:32 +0700

    Update EditProfile.jsx

--- src/components/navigation/EditProfile.jsx
+++ src/components/navigation/EditProfile.jsx
@@ -10,7 +10,7 @@ function EditProfile({ edit }) {
   const [show, setShow] = useState(false);
   const [save, setSave] = useState(true);
 
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
@@ -27,11 +27,11 @@ function EditProfile({ edit }) {
     return useToken?.token;
   }
 
-  console.log(getAccessToken())
+  console.log(getAccessToken());
 
   const handleSave = (event) => {
     event.preventDefault();
-    console.log('clicked')
+    console.log("clicked");
     const data = new FormData();
     data.append("name", name);
     data.append("address", address);
@@ -42,7 +42,8 @@ function EditProfile({ edit }) {
     if (save === true) {
       axios
         .post(
-          `http://pitrash.masuk.web.id/api/user/edit`, data,
+          `http://pitrash.masuk.web.id/api/user/edit`,
+          data,
 
           {
             headers: {
@@ -56,13 +57,11 @@ function EditProfile({ edit }) {
           // console.log(response.data.data.author);
           setSave(response.data.data);
           console.log(response.data.data);
-          alert('berhasil di ubah')
-          navigate('/profile')
+          alert("berhasil di ubah");
+          navigate("/profile");
         });
     }
   };
-
-
 
   return (
     <>
@@ -71,24 +70,63 @@ function EditProfile({ edit }) {
       </Button>
       <Modal show={show} onHide={handleClose} style={{ fontFamily: "Manrope" }}>
         <Modal.Header closeButton>
-          <Modal.Title style={{ fontFamily: "Museo" }}>Edit Profile</Modal.Title>
+          <Modal.Title style={{ fontFamily: "Museo" }}>
+            Edit Profile
+          </Modal.Title>
         </Modal.Header>
         <Modal.Body>
           <Form onSubmit={(e) => handleSave(e)}>
-            <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
-              <Form.Control type="text" placeholder="nama" defaultValue={edit.name} onChange={(e) => setName(e.target.value)} />
+            <Form.Group
+              className="mb-2"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                type="text"
+                placeholder="nama"
+                defaultValue={edit.name}
+                onChange={(e) => setName(e.target.value)}
+              />
             </Form.Group>
-            <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
-              <Form.Control type="email" placeholder="email" defaultValue={edit.email} onChange={(e) => setEmail(e.target.value)} />
+            <Form.Group
+              className="mb-2"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                type="email"
+                placeholder="email"
+                defaultValue={edit.email}
+                onChange={(e) => setEmail(e.target.value)}
+              />
             </Form.Group>
-            <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
-              <Form.Control type="number" placeholder="telepon" defaultValue={edit.phone} onChange={(e) => setPhone(e.target.value)} />
+            <Form.Group
+              className="mb-2"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                type="number"
+                placeholder="telepon"
+                defaultValue={edit.phone}
+                onChange={(e) => setPhone(e.target.value)}
+              />
             </Form.Group>
-            <Form.Group className="mb-2" controlId="exampleForm.ControlTextarea1">
-              <Form.Control as="textarea" placeholder="Alamat" rows={3} defaultValue={edit.user_detail?.address} onChange={(e) => setAddress(e.target.value)} />
+            <Form.Group
+              className="mb-2"
+              controlId="exampleForm.ControlTextarea1"
+            >
+              <Form.Control
+                as="textarea"
+                placeholder="Alamat"
+                rows={3}
+                defaultValue={edit.user_detail?.address}
+                onChange={(e) => setAddress(e.target.value)}
+              />
             </Form.Group>
             <Form.Group controlId="formFile" className="mb-3">
-              <Form.Control type="file" accept=".jpg, .png" onChange={(e) => setImage(e.target.files[0])} />
+              <Form.Control
+                type="file"
+                accept=".jpg, .png"
+                onChange={(e) => setImage(e.target.files[0])}
+              />
               <Button onClick={handleSave} variant="primary" className="mt-2">
                 Simpan
               </Button>
@@ -101,7 +139,6 @@ function EditProfile({ edit }) {
               <Button variant="danger" onClick={handleClose}>
                 Batal
               </Button>
-
             </Modal.Footer>
           </Form>
         </Modal.Body>
