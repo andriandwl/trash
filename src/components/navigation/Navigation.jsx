dbe53b676b24e32fb53b561cef4aa421cca49ab7
andriandwi <andriandwiharyantoe@gmail.com>
2023-03-01 10:19:31 +0700

    last

--- src/components/navigation/Navigation.jsx
+++ src/components/navigation/Navigation.jsx
@@ -30,7 +30,7 @@ function Navigation() {
                 className="collapse navbar-collapse"
                 id="navbarSupportedContent"
               >
-                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                <ul className="navbar-nav  me-auto mb-2 mb-lg-0">
                   <li className="nav-item ">
                     <Link
                       className="nav-link active text-light"
